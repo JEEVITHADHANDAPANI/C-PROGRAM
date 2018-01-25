aa6be894864ade6c091fc1611c96beb3222ff06b
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2018-01-25 19:53:29 +0530

    cel.c

---
+++ cel.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int cel,kel;
+  printf("Enter the value in celusius");
+  scanf("%d",&cel);
+  kel=cel+273;
+  printf("Temparature in kelvin is%d",kel);
+  return 0;
+}
