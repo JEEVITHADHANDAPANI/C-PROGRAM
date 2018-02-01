0d94a5f35444277c3cefbb8dd09c522786253476
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2018-02-01 07:46:34 +0000

    armstrong between intervals.c

---
+++ armstrong between intervals.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,sum,t,i,n;
+  printf("Enter the intervals:");
+  scanf("%d\t%d",&a,&b);
+  for(i=a;i<=b;i++)
+  {
+    sum=0;
+    n=i;
+    while(n!=0)
+    {
+      t=n%10;
+      sum=sum+(t*t*t);
+      n=n/10;
+    }
+    if(sum==i)
+    {
+      printf("%d",i);
+    }
+  }
+  return 0;
+}
