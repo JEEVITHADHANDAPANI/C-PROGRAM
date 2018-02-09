0552eb87803dc21e130c76c0f5facc3643a36ed6
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2018-02-09 14:56:26 +0530

    index.c

---
+++ index.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+  int n,a[10],i,j,count=0;
+  printf("Enter no of elements:");
+  scanf("%d",&n);
+  printf("Enter elements:");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    if(a[i]==i)
+    {
+      printf("%d\t",a[i]);
+    }
+    else
+    {
+      count++;
+    }
+  }
+ if(count==n)
+ {
+   printf("-1");
+ }
+  return 0;
