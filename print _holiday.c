3085121a8cb831cb31a91826b8651711bdea610e
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2018-02-13 16:09:38 +0530

    print_holiday.c

---
+++ print _holiday.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  char a[10];
+  printf("Enter the string:");
+    scanf("%d",a);
+    if(a=="saturday"||a=="sunday")
+    {
+    printf("YES");
+    }
+  else
+  {
+    printf("NO");
+  }
+  return 0;
+}
