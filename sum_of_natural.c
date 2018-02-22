9a67c9d1807e3c55975b5f425f5db5078595b4c7
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2018-02-22 10:29:34 +0000

    sum_of_natural.c

---
+++ sum_of_natural.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,sum=0;
+    printf("Enter number:\n");
+    scanf("%d",&n);
+    for(i=1;i<=a;i++)
+    {
+        sum=sum+i;
+    }
+    printf("%d",sum);
+    return 0;
+}
