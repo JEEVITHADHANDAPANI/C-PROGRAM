3308c40817476b83c5e59b00b14e5f8a06261ffb
JEEVITHADHANDAPANI <30828391+JEEVITHADHANDAPANI@users.noreply.github.com>
2018-03-20 16:37:14 +0530

    delete_k.c

---
+++ delete_k.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],m,k,i;
+    scanf("%d\t%d",&m,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<m-k;i++)
+    {
+     printf("%d ",a[i]);   
+    }
+    return 0;
+}
